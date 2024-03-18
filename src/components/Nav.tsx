94c363c68505d0fd641ea36851c43982e410e388
Rafaella Fernández <54990883+chanmi-07@users.noreply.github.com>
2024-03-17 21:05:47 -0500

    Update Nav component styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,7 +13,7 @@ const headerLinks: HeaderLink[] = HeaderLinks;
 
 export default function Header(props: Props) {
   const { children } = props;
-  return <header className="fixed w-screen">
+  return <header className="fixed w-screen z-10">
     <nav className="w-[1600px] max-w-full mx-auto flex justify-between items-center py-2 px-4">
       {children}
     </nav>
