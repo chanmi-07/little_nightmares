48abf7db25f0cb805aefddb89bfd5c5667d708cf
Rafaella Fernández <54990883+chanmi-07@users.noreply.github.com>
2024-03-20 14:42:03 -0500

    Remove unused imports and interfaces from Nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import HeaderLinks from "../data/header_links.json";
-import Logo from "../icons/icon.svg";
-
-interface HeaderLink {
-  name: string;
-  link: string;
-}
 
 type Props = React.ComponentProps<"div">;
-
-const headerLinks: HeaderLink[] = HeaderLinks;
 
 export default function Header(props: Props) {
   const { children } = props;
